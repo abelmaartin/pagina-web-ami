464b3b65e7b708984ef7a7bf2c47cc3e0efbafd6
Abel Martín Meneses <alu0101660011@ull.edu.es>
2026-08-02 10:04:23 +0100

    Corregido el adaptador de Prisma a Postgres

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaLibSql } from '@prisma/adapter-libsql';
-import path from 'path';
-import { pathToFileURL } from 'url';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const prismaClientSingleton = () => {
-  // 1. Calculamos la ruta absoluta a la base de datos de forma segura
-  const dbPath = path.join(process.cwd(), 'prisma', 'dev.db');
-  const dbUrl = pathToFileURL(dbPath).toString();
-
-  // 2. ¡EL CAMBIO CLAVE! Le pasamos el objeto Config directamente al adaptador.
-  // Cero dependencias de createClient, cero errores de TypeScript.
-  const adapter = new PrismaLibSql({
-    url: dbUrl
-  });
-
-  // 3. Devolvemos el cliente listo para funcionar
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  
   return new PrismaClient({ adapter });
 };
 
